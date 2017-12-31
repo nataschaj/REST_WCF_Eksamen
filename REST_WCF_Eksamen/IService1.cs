699f36cd94a9359fc623c140e027bd9e5b1beb71
natsachaj <natascha_jakobsen@hotmail.com>
2017-12-31 15:03:40 +0100

    e

--- REST_WCF_Eksamen/IService1.cs
+++ REST_WCF_Eksamen/IService1.cs
@@ -6,6 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
+//for eksamen
 namespace REST_WCF_Eksamen
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
